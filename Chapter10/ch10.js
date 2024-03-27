3e238f4bb09db049b11721be88d52c0b8f8959cb
Desire Gwanzura <112183262+GuanKid@users.noreply.github.com>
2024-03-27 07:28:10 +0200

    Add files via upload

---
+++ Chapter10/ch10.js
@@ -0,0 +1,15 @@
+//console.dir(document.body);
+
+
+
+// let bodyChildren = document.body.children;
+// let firstChild = bodyChildren[0];
+// const thirdChild = bodyChildren[2];
+// console.log(firstChild);
+// console.log(thirdChild);
+
+
+// let ulElement = document.querySelector('ul');
+// let nextSibling = ulElement.nextElementSibling;
+// console.log(nextSibling);
+
